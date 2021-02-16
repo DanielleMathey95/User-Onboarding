b958084b3e5ad38d6de8b6ed57c8835445c13ff6
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-02-16 15:44:25 -0800

    set up useEffect and input change for form

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import * as yup from 'yup';
 import axios from 'axios';
 
+
+
 export default function Form() {
   const [formState, setFormState] = useState({
     name: '',
@@ -17,5 +19,24 @@ export default function Form() {
     terms: ''
   });
 
-  
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Name is a required field"),
+    email: yup.string().email("Must be a valid email address").required(),
+    password: yup.string().min(8).required("Must be at least 8 characters"),
+    terms: yup.boolean().oneOf([true], "Do you accept these terms & conditions?")   
+})
+
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      console.log('valid?', valid);
+    })
+  }, [formState])
+
+  const inputChange = event => {
+    const newFormData = {
+      ...formState,
+      [event.target.name] : event.target.type === "checkbox" ? event.target.checked : event.target.value
+    };
+    setFormState(newFormData)
+  }
 }
